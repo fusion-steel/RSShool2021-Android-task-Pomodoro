cfb34dd8fc714c0c4e65aea57ceb33b4f450cfa8
fusion-steel <pavelgently@gmail.com>
2021-07-13 23:33:24 +0300

    add: MainActivity

--- app/src/main/java/com/example/rsshool2021_android_task_pomodoro/MainActivity.kt
+++ app/src/main/java/com/example/rsshool2021_android_task_pomodoro/MainActivity.kt
@@ -2,16 +2,66 @@ package com.example.rsshool2021_android_task_pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rsshool2021_android_task_pomodoro.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), StopwatchListener {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val stopwatchAdapter = StopwatchAdapter(this)
+    private val stopwatches = mutableListOf<Stopwatch>()
+    private var nextId = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = stopwatchAdapter
+        }
+
+        binding.addButton.setOnClickListener {
+            println("add new stopwatch at index $nextId")
+            stopwatches.add(Stopwatch(nextId++, 0, false))
+            notifyChanges()
+        }
     }
 
+    override fun start(id: Int) {
+        println("start stopwatch timer at index $id")
+        changeStopwatch(id, null, true)
+    }
+
+    override fun stop(id: Int, currentMs: Long) {
+        println("stop stopwatch timer at index $id")
+        changeStopwatch(id, currentMs, false)
+    }
+
+    override fun delete(id: Int) {
+        println("delete stopwatch at index $id")
+        stopwatches.remove(stopwatches.find { it.id == id })
+        notifyChanges()
+    }
+
+    private fun changeStopwatch(id: Int, currentMs: Long?, isStarted: Boolean) {
+        val stopwatch = stopwatches.find { it.id == id }
+
+        stopwatch?.apply {
+            if (currentMs != null) {
+                this.currentMs = currentMs
+            }
+            this.isStarted = isStarted
+        }
+
+        notifyChanges()
+    }
+
+    private fun notifyChanges() {
+        stopwatchAdapter.notifyDataSetChanged()
+        stopwatchAdapter.submitList(stopwatches.toList())
+    }
 }
