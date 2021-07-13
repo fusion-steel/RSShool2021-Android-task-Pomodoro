d797662785dc57a4993cb4f9bc42325d3cbe11a8
fusion-steel <pavelgently@gmail.com>
2021-07-13 23:32:31 +0300

    add: Stopwatch view holder

---
+++ app/src/main/java/com/example/rsshool2021_android_task_pomodoro/StopwatchViewHolder.kt
@@ -0,0 +1,106 @@
+package com.example.rsshool2021_android_task_pomodoro
+
+import android.content.res.Resources
+import android.graphics.drawable.AnimationDrawable
+import android.os.CountDownTimer
+import androidx.core.view.isInvisible
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rsshool2021_android_task_pomodoro.databinding.StopwatchItemBinding
+
+class StopwatchViewHolder(
+    private val binding: StopwatchItemBinding,
+    private val listener: StopwatchListener,
+    private val resources: Resources
+) : RecyclerView.ViewHolder(binding.root) {
+
+    private var timer: CountDownTimer? = null
+
+    fun bind(stopwatch: Stopwatch) {
+        binding.stopwatchTimer.text = stopwatch.currentMs.displayTime()
+
+        if (stopwatch.isStarted) {
+            startTimer(stopwatch)
+        } else {
+            stopTimer(stopwatch)
+        }
+
+        initButtonsListeners(stopwatch)
+    }
+
+    private fun initButtonsListeners(stopwatch: Stopwatch) {
+        binding.startStopButton.setOnClickListener {
+            if (stopwatch.isStarted) {
+                listener.stop(stopwatch.id, stopwatch.currentMs)
+            } else {
+                listener.start(stopwatch.id)
+            }
+        }
+
+        binding.deleteButton.setOnClickListener { listener.delete(stopwatch.id) }
+    }
+
+    private fun startTimer(stopwatch: Stopwatch) {
+        val stop = resources.getString(R.string.stop)
+        binding.startStopButton.text = stop
+
+        timer?.cancel()
+        timer = getCountDownTimer(stopwatch)
+        timer?.start()
+
+        binding.blinkingIndicator.isInvisible = false
+        (binding.blinkingIndicator.background as? AnimationDrawable)?.start()
+    }
+
+    private fun stopTimer(stopwatch: Stopwatch) {
+        val start = resources.getString(R.string.start)
+        binding.startStopButton.text = start
+
+        timer?.cancel()
+
+        binding.blinkingIndicator.isInvisible = true
+        (binding.blinkingIndicator.background as? AnimationDrawable)?.stop()
+    }
+
+
+    private fun getCountDownTimer(stopwatch: Stopwatch): CountDownTimer {
+        return object : CountDownTimer(PERIOD, UNIT_TEN_MS) {
+            val interval = UNIT_TEN_MS
+
+            override fun onTick(millisUntilFinished: Long) {
+                stopwatch.currentMs += interval
+                binding.stopwatchTimer.text = stopwatch.currentMs.displayTime()
+            }
+
+            override fun onFinish() {
+                binding.stopwatchTimer.text = stopwatch.currentMs.displayTime()
+            }
+        }
+    }
+
+    private fun Long.displayTime(): String {
+        if (this <= 0L) {
+            return START_TIME
+        }
+        val h = this / 1000 / 3600
+        val m = this / 1000 % 3600 / 60
+        val s = this / 1000 % 60
+        val ms = this % 1000 / 10
+
+        return "${displaySlot(h)}:${displaySlot(m)}:${displaySlot(s)}:${displaySlot(ms)}"
+    }
+
+    private fun displaySlot(count: Long): String {
+        return if (count / 10L > 0) {
+            "$count"
+        } else {
+            "0$count"
+        }
+    }
+
+    private companion object {
+
+        private const val START_TIME = "00:00:00:00"
+        private const val UNIT_TEN_MS = 10L
+        private const val PERIOD  = 1000L * 60L * 60L * 24L
+    }
+}
